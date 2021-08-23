9e9119a2755f64065219c8a2e7a7463229a9799d
Swopnab <89373272+Swopnab@users.noreply.github.com>
2021-08-23 20:26:02 +0545

    Update planning_1.py

--- planning_1.py
+++ planning_1.py
@@ -1,16 +1,20 @@
 """
 tic tac toe board
 [
-  [x, -, -],
+  [-, -, -],
   [-, -, -],
   [-, -, -]
 ]
-user_input -> something 1-9
-if they enter anything else: tell them too go again
-check if the user_input is already taken
+
+user_input ---> number from 1 - 9
+if not from 1 - 9 then type again
+if the number is inputted then check if the number is taken or not
+differ 2 users as 'X' and 'O'
 add it to the board
-check if user won: checking rows, columns and diagonals
+check if user won or lost or tie
+checking rows, columns and diagonals to determine
 toggle between users upon succesful moves
+
 """
 
 
